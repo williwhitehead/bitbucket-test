6bec3bab588fb2c2d4ec7c1fbb4083b6db36be55
Stefan Saasen <ssaasen@atlassian.com>
2015-02-20 12:16:58 +1100

    Check that version is not nil

--- scripts/update_image.rb
+++ scripts/update_image.rb
@@ -29,7 +29,7 @@ class Docker
   end
 
   def update_version_on_branch!(branch, version)
-    raise "Version is empty" if version.strip.empty?
+    raise "Version is empty" if version.nil? || version.strip.empty?
     with_branch(branch) do 
       content = IO.readlines("Dockerfile")
       File.open("Dockerfile", "w") do |f|
@@ -119,7 +119,7 @@ def determine_required_updates
   to_update = {}
   current_versions_in_docker.each do |k,v|
     stash_latest = k.eql?("master") ? stash.most_recent_version : stash.most_recent_version_matching(k.gsub(/release\//,''))
-    unless stash_latest.eql?(v) || stash_latest.strip.empty?
+    unless stash_latest.eql?(v) || stash_latest.nil? || stash_latest.strip.empty?
       to_update[k] = stash_latest
     end
   end

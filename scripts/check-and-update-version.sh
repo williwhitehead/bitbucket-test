f1ec334bee8b77dd5191f340d946161b81e313f1
Sebastian Cole <secole@atlassian.com>
2015-08-25 12:34:37 +1000

    Renaming release scripts, stash->bitbucket

--- scripts/check-and-update-version.sh
+++ scripts/check-and-update-version.sh
@@ -9,22 +9,22 @@ ret=$?
 if [ "$ret" == "1" ]; then
 
     # Build docker image, use a test tag to not accidentially use the canonical version
-    sudo docker build -t="atlassian/stash-test" .
+    sudo docker build -t="atlassian/bitbucket-test" .
     # Sanity check, list images
     docker images
 
     # Test the running container
-    git show scripts:scripts/test-stash-status.sh > test-stash-status.sh
-    chmod u+x ./test-stash-status.sh
+    git show scripts:scripts/test-bitbucket-status.sh > test-bitbucket-status.sh
+    chmod u+x ./test-bitbucket-status.sh
 
     # Ensure permissions are correct
-    sudo docker run -u root -v /data/stash:/var/atlassian/application-data/stash atlassian/stash-test chown -R daemon  /var/atlassian/application-data/stash
+    sudo docker run -u root -v /data/stash:/var/atlassian/application-data/bitbucket atlassian/bitbucket-test chown -R daemon  /var/atlassian/application-data/bitbucket
 
     # Start Stash
-    sudo docker run -v /data/stash:/var/atlassian/application-data/stash --name="stash" -d -p 7990:7990 -p 7999:7999 atlassian/stash-test
+    sudo docker run -v /data/stash:/var/atlassian/application-data/bitbucket --name="bitbucket" -d -p 7990:7990 -p 7999:7999 atlassian/bitbucket-test
 
 
-    ./test-stash-status.sh
+    ./test-bitbucket-status.sh
     curl -v "http://localhost:7990/status"
 
     # Push changes
--- scripts/test-bitbucket-status.sh
+++ scripts/test-bitbucket-status.sh
@@ -1,12 +1,12 @@
 URL="http://localhost:7990/status"
 
-echo "Wait for the container to come up and Stash to start"
+echo "Wait for the container to come up and Bitbucket to start"
 until $(curl --output /dev/null --silent --head --fail $URL); do
     printf '.'
     sleep 5
 done
 
-# Now the container is up and running and Stash is running. Let's wait until it shows the setup screen
+# Now the container is up and running and Bitbucket is running. Let's wait until it shows the setup screen
 echo "Check the /status output"
 curl -v $URL | grep FIRST_RUN
 ret=$?
@@ -14,6 +14,6 @@ if [ "$ret" != "0" ]; then
     echo "Unexpected /status response"
     exit $ret
 else
-    echo "Stash seems to be up and running"
+    echo "Bitbucket seems to be up and running"
     exit 0
 fi

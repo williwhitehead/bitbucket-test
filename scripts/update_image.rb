66a17182f944b2f1ac9a5c64baa62e404c4c59bc
Stefan Petrucev <spetrucev@atlassian.com>
2016-07-20 15:03:22 +1000

    BSERVDEV-13374: Throw if Bitbucket Server contains no release tags
    The Bitbucket Server repository should always contain tags on the Maven commit for each release. If the STASH/stash linked repository in Bamboo is configured to only do a shallow clone, then these tags will not show up. Currently, the script will continue and misleadinging report that there is no new version to update, even though this may be not true. This change will cause the script to fail early if the tags list is empty (which shouldn't ever happen if the linked repository is configured correctly).

--- scripts/update_image.rb
+++ scripts/update_image.rb
@@ -123,6 +123,9 @@ class Stash
   def resolve_stash_versions
     with_repo(@repo) do
       tags = `git tag`.split.select {|tag| tag =~ /bitbucket-parent/}.collect{|tag| tag.gsub(/bitbucket-parent-/,"")}.select{|version| version =~ /(\d+\.\d+(.\d+)?$)/}
+      if tags.empty?
+        raise "Tags list is empty"
+      end
       tags.sort_by do |version|
         if version =~ /^(\d+)\.(\d+)\.(\d+).*/
           major, minor, patch = $1.dup.to_i, $2.dup.to_i, $3.dup.to_i
